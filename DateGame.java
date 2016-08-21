d204990ef9fa2aaa313c2fda1a84c64708e0e94d
Matheus Clemente Bafutto <matheusbafutto@gmail.com>
2016-08-21 12:02:27 -0300

    Delete DateGame.java
    deleted

--- DateGame.java
+++
@@ -1,9 +0,0 @@
-import java.time.Instant;
-public class DateGame {
-    public static void main(String args[]) {
-        String date = "2016-08-29T02:48:28Z";
-        int interval = 134312;
-        Instant t = Instant.parse(date);
-        System.out.println(t.plusSeconds(interval));
-    }
-}
