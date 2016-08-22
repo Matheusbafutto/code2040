422789692937faf9b436d57bafa1314de07e0076
Matheus Clemente Bafutto <matheusbafutto@gmail.com>
2016-08-22 09:19:35 -0300

    remiving old Sender directory

--- Sender/src/DateGame.java
+++
@@ -1,9 +0,0 @@
-import java.time.Instant;
-public class DateGame {
-    public static String api4Call(String date, int interval) {
-        Instant t = Instant.parse(date);
-        String ans = t.plusSeconds(interval).toString();
-        System.out.println(ans);
-        return ans;
-    }
-}
